ea0803a84440d0198469b2d47efdc2f630e0f531
rachad <rachdos76@gmail.com>
2026-02-27 22:35:02 +0000

    creer la methode statsdashboard pour afficher les statisques pour l'admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Colocation;
+use App\Models\Depense;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function index()
+    {
+        $stats = [
+            'users_count' => User::count(),
+            'colocs_count' => Colocation::count(),
+            'total_money' => Depense::sum('montant'),
+            'banned_count' => User::where('est_actif', false)->count(),
+        ];
+
+        $users = User::with('colocation')->latest()->paginate(10);
+
+        return view('admin.dashboard', compact('stats', 'users'));
+    }
 }
